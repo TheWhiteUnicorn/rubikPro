996e8ba4daac564d3502504908353aad2385167b
MrValyk <valik.sparov.ru1@gmail.com>
2017-10-25 00:12:27 +0300

    плановый

--- Rubik Pro/cv/constants.h
+++ Rubik Pro/cv/constants.h
@@ -12,8 +12,8 @@ const int WINDOW_HEIGTH = 480;
 const int FRAME_MID_X = WINDOW_WIDTH / 2;
 const int FRAME_MID_Y = WINDOW_HEIGTH / 2;
 
-const int FRAME_W = 120;
-const int FRAME_H = 120;
+const int FRAME_W = 200;
+const int FRAME_H = 200;
 
 const vector<Point> frame = {{ FRAME_MID_X - FRAME_W / 2, FRAME_MID_Y - FRAME_H / 2},
 							 { FRAME_MID_X + FRAME_W / 2, FRAME_MID_Y + FRAME_H / 2} };
